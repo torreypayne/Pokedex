c3a009b67761a489d816febec500e0ee46d64e3c
App Academy Student <student@appacademy.io>
2015-06-01 16:42:15 -0700

    Finish Phase 2; Create basic Pokedex

--- skeleton/app/assets/javascripts/pokedex-1B.js
+++ skeleton/app/assets/javascripts/pokedex-1B.js
@@ -1,12 +1,25 @@
 Pokedex.RootView.prototype.renderPokemonDetail = function (pokemon) {
+
+  var detail = this;
   var $div = $('<div>').addClass('detail');
   $div.append($('<img>').attr('src', pokemon.get('image_url')));
   for (var attr in pokemon.attributes) {
     var $p = $('<p>').append(attr + ": ");
     $div.append($p.append(pokemon.escape(attr)));
   }
+  var toyList = $('<ul>').addClass('toys');
+  pokemon.fetch({
+    success: function() {
+      pokemon.toys().each(function(toy) {
+        detail.addToyToList(toy);
+      });
+    }
+  });
+
+  $div.append(toyList);
 
   this.$pokeDetail.html($div);
+  this.$pokeDetail.on("click", "li", this.selectToyFromList.bind(this));
 };
 
 Pokedex.RootView.prototype.selectPokemonFromList = function (event) {
--- skeleton/app/assets/javascripts/pokedex-2.js
+++ skeleton/app/assets/javascripts/pokedex-2.js
@@ -1,8 +1,23 @@
 Pokedex.RootView.prototype.addToyToList = function (toy) {
+  var $li = $('<li>').data('toy-id', toy.get('id'));
+  $li.data('pokemon-id', toy.get('pokemon_id'));
+  var $attrs = $('<p>');
+  $attrs.text(toy.get('name') + ' ' + toy.get('happiness') + ' ' + toy.get('price'));
+  $li.append($attrs);
+  this.$pokeDetail.find('ul').append($li);
 };
 
 Pokedex.RootView.prototype.renderToyDetail = function (toy) {
+  var $toyDetail = $('div').addClass('toy-detail');
+  this.$toyDetail.html($toyDetail);
 };
 
 Pokedex.RootView.prototype.selectToyFromList = function (event) {
+  var $selectedToy = $(event.currentTarget);
+  var pokemon = this.pokes.find({ id: $selectedToy.data('pokemon-id') });
+
+  var toy = pokemon.toys().find({ id: $selectedToy.data('toy-id') });
+
+  this.$toyDetail.html( $('<div>').addClass('detail').append(toy.get('name')) );
+  this.$toyDetail.append($('<img>').attr('src', toy.get('image_url')));
 };
